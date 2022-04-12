f6bd0bd1c69ea43f1fbc6ad6bb982f45db188d9b
JserCai <localhost_8080@163.com>
2022-04-12 17:26:11 +0800

    提交测试

---
+++ test/index.test.ts
@@ -0,0 +1,18 @@
+/*
+ * @Description: 测试
+ * @Author: JserCai
+ * @Date: 2022-04-12 17:25:30
+ * @LastEditTime: 2022-04-12 17:25:30
+ * @LastEditors: JserCai
+ */
+import { random } from '../src/index'
+describe('生成数字范围内的随机数', () => {
+  it('random(1, 1) -> should return 1', () => {
+    const rand = random(1, 1)
+    expect(rand).toBe(1)
+  })
+  it('random(1, 10) -> should return number', () => {
+    const rand = random(1, 10)
+    expect(rand).toBeNaN
+  })
+})
